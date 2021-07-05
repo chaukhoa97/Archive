62543c97b44aa4d391bca11f422490e7fab826ba
jonhkane00 <jonhkane00@gmail.com>
2021-07-05 21:47:11 +0700

    k

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -2,23 +2,29 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
+import { useState } from 'react';
 
 //! Hai <div> __description & __price KHÔNG nhận className "card" và "expense-item"
-//* Hai thằng lần lượt dc chúng ta pass vào className "ei__desciprtion" và "ei__price" để style
+//* Hai thằng lần lượt dc chúng ta pass vào className "e-i__desciprtion" và "e-i__price" để style
 function ExpenseItem(props) {
+  const [count, setCount] = useState(0); //* const [state, setState] = useState(intialState)
+  function clickHandler() {
+    setCount(count + 1);
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{props.title + count}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
 
 //? props Là attribute của các React component: <ExpenseItem date = '1' title = '2'></ExpenseItem> -> props.date = 1; props.title = 2;
-// Bên Expenses.css: ExpenseItem dc truyền date vào -> ExpenseItem.date
+// Bên Expenses.jsx: <ExpenseItem date = "..."> -> ExpenseItem.date
 //* Ở line 10: <ExpenseDate date = {props.date} /> ~~ ExpenseDate.date = ExpenseItem.date
 
 export default ExpenseItem;
--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -6,11 +6,12 @@ function Card(props) {
   return <div className={'card ' + props.className}>{props.children}</div>;
 }
 
-//! <Card> sẽ render đám {props.children} trong một <div>, <div> đó sẽ có 2 className:
-//! Chỉ có <div> đó dc nhận style thôi, đám children ở trong không dc nhận (muốn có thì tự nhét className vào, xem ExpenseItme.jsx line 6)
+//? <Card> sẽ render đám {props.children} trong một <div>, <div> đó sẽ có 2 className:
 //* Một là "card" để có border-radius và box-shadow
 //* Hai là props.className để styling cho riêng từng children (chi tiết hơn ở Expenses.jsx):
-//    Expenses.jsx và ExpenseItem đều cần border-radius và box shadow của "card" ở trên, nhưng 2 thằng đó có những style riêng
-//    -> props.className sẽ styling cho cái div chứa đám children trong Expenses & ExpenseItem
+//    <div> của Expenses.jsx và ExpenseItem đều đã có border-radius và box shadow của "card" ở trên,
+//    props.className sẽ styling thêm cho 2 cái <div> trên, vì 2 thằng có những style riêng
+//! Chỉ có <div> đó dc nhận style, đám children ở trong KHÔNG dc nhận
+//!     (muốn có thì tự nhét className vào, xem ExpenseItem.jsx line 6)
 
 export default Card;
