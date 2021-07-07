3050660ef73c32fb92bf74499db1deba66d3f3be
chaukhoa97 <70827148+chaukhoa97@users.noreply.github.com>
2021-07-07 10:42:13 +0700

    a

---
+++ src/Storage/List-and-Keys.jsx
@@ -0,0 +1,18 @@
+function ListItem(props) {
+  return <li>{props.value}</li>;
+}
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  //* A good rule of thumb is that elements inside the map() call need keys.
+  // Key của 1 element (trong 1 array) là thứ có giá trị riêng biệt với các element khác (siblings) của nó,
+  //    -> nên dùng ID từ dữ liệu -> dùng chính value từ dữ liệu làm key -> cuối cùng là index (không nên)
+  const listItems = numbers.map((number) => (
+    <ListItem key={number.toString()} value={number} />
+  ));
+  return <ul>{listItems}</ul>;
+}
+
+const numbers = [1, 2, 3, 4, 5];
+
+export default NumberList;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+import Greeting from './Storage/Conditional';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  // Dùng "" cho string, dùng {} cho expression
+  [<App />, <Greeting isLoggedIn="" />],
+  document.getElementById('root')
+);
