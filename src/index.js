6654189f9ae1ea39ebf10a69e1fb7bd704640b54
chaukhoa97 <70827148+chaukhoa97@users.noreply.github.com>
2021-07-07 16:47:43 +0700

    a

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+
 import Greeting from './Storage/Conditional';
+import Example from './Storage/Effect-hook';
 
 ReactDOM.render(
   // Dùng "" cho string, dùng {} cho expression
-  [<App />, <Greeting isLoggedIn="" />],
+  [<App />, <Greeting isLoggedIn="" />, <Example />],
   document.getElementById('root')
 );
