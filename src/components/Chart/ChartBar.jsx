122524e2e273016cd515072a91f7154f1962c34c
jonhkane00 <jonhkane00@gmail.com>
2021-08-02 01:20:21 +0700

    a

---
+++ src/components/Chart/ChartBar.jsx
@@ -0,0 +1,25 @@
+import './ChartBar.css';
+
+const ChartBar = (props) => {
+  let barFillHeight = '0%';
+
+  if (props.maxValue > 0) {
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%';
+  }
+
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div
+          className="chart-bar__fill"
+          // Dấu {} bên ngoài -> expression; dấu {} bên trong -> style object in React
+          // Expression:  Let the JSX parser know that it needs to interpret the contents in between them as JavaScript instead of a string
+          style={{ height: barFillHeight }}
+        ></div>
+      </div>
+      <div className="chart-bar__label">{props.label}</div>
+    </div>
+  );
+};
+
+export default ChartBar;
