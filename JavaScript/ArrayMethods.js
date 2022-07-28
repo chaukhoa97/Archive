820ef9145d2883b653b8939e3b2583cae4989dab
Finn Chau <khoacd@smartosc.com>
2022-07-28 14:32:49 +0700

    a

--- JavaScript/ArrayMethods.js
+++ JavaScript/ArrayMethods.js
@@ -1,46 +1,39 @@
-//! Mutate array cũ (A' === A --> TRUE)
+//! Mutate array cũ (A' === A -> TRUE)
 //* Return value mới (Ko mutate array gốc)
 
 //! Basics
 const numbers = [3, 4];
-numbers.push(5, 6); // Thêm vào cuối - [3, 4, 5, 6]
-numbers.pop(); // Xóa số cuối - [3, 4, 5]
-numbers.unshift(1, 2); // Thêm vào đầu - [1, 2, 3, 4, 5]
-numbers.shift(); // Xóa số đầu - [2, 3, 4, 5]
-numbers.reverse(); // Đảo ngược - [5, 4, 3, 2]
+numbers.push(5, 6); // Thêm vào cuối
+numbers.pop(); // Xóa 1 số cuối
+numbers.unshift(1, 2); // Thêm vào đầu
+numbers.shift(); // Xóa 1 số đầu
+numbers.reverse(); // Đảo ngược
 numbers.splice(2, 1, "a", "b"); // (fromIndex, deleteCount, item1, item2, ...); returns an array containing the deleted items
-
-//* Set
-uniqueArray = [...new Set(array)];
-
-//* Filters, every, some - Same arguments: .filter((value,index,array)=>{...})
-let numbers = [1, 2, 3, 4];
-const allPositive = numbers.every((value) => value >= 0); // true
-const atLeastOneNegative = numbers.some((value) => value <= 0); // false
-const f = numbers.filter((n) => n > 5); //* New array
-
-//* .concat & .slice Array
-const first = [1, 2, 3];
-const second = [4, 5, 6];
-const combined = first.concat(second); // [1, 2, 3, 4, 5, 6]
-const sliced = combined.slice(2, 5); // Ko tính end; cũng có thể dùng .slice() để shallow copy
-
-//1 Map & forEach
-const arr = [1, 2, 3, 4, 5];
-//* Map: Creating a NEW array containing output of some processing done on each element of the array.
-const mappedArray = arr.map((value, index, array) => element.children);
-
-//! forEach: Executes a provided function once per array element and Returns `undefined`. For example, saving all elements in the database.
-arr.forEach(
-  (currentValue, index, arr) => arr[index] === currentValue, //! Dummy callback
-  thisValue // The value used as the function's "this" value
-);
-
 //! Sort
 // a - b <= 0 -> a, b giữ nguyên vị trí (vẫn a xếp trước b), còn > 0 thì đổi chỗ
 anotherCombined.sort((a, b) => a - b);
 // Sort theo text alphabet
-users.sort((a, b) => a.firstname.localeCompare(b.firstname));
+users.sort((a, b) => a.localeCompare(b));
+//! forEach: Executes a provided function once per array element and Returns `undefined`. For example, saving all elements in the database.
+const arr = [1, 2, 3, 4, 5];
+arr.forEach(
+  (currentValue, index, arr) => arr[index] === currentValue, //! Dummy callback
+  thisValue // The value used as the function's "this" value
+);
+//* Map: Creating a NEW array containing output of some processing done on each element of the array.
+const mappedArray = arr.map((value, index, array) => element.children);
+
+//* Set
+uniqueArray = [...new Set(array)];
+
+//* Filters, every, some - Same arguments: .filter((value, index, array)=>{...})
+const allPositive = numbers.every((value) => value >= 0); // true
+const atLeastOneNegative = numbers.some((value) => value <= 0); // false
+const f = numbers.filter((n) => n > 5); //* Returns new array
+
+//* .concat & .slice
+const combined = [1, 2].concat([3, 4]); // [1, 2, 3, 4]
+const sliced = combined.slice(1, 3); // Index 1 2 (ko lấy 3); cũng có thể dùng .slice() để shallow copy
 
 //* Joining & Spliting, first = [1, 2, 3]
 const joined = first.join("+"); // Return string "1+2+3"
@@ -67,12 +60,6 @@ const summ = [15, 16, 17].reduce(
 var arr2 = [1, 2, [3, 4, [5, 6]]];
 arr2.flat(); // [1, 2, 3, 4, [5, 6]]
 arr2.flat(2); // [1, 2, 3, 4, 5, 6]
-
-//* Move item in array to last position
-// Already know the index of the item to move
-array.push(array.splice(index, 1)[0]);
-// If you don't have the index, and only the element
-array.push(array.splice(array.indexOf(element), 1)[0]);
 
 //1 Lodash
 var _ = require("lodash");
