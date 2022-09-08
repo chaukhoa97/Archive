c716c445256ee551e2e4749ff4f35ffa1da021ab
Finn Chau <khoacd@smartosc.com>
2022-09-08 17:40:06 +0700

    a

--- TypeScript/types.ts
+++ TypeScript/types.ts
@@ -1,43 +1,13 @@
-//1 Type Assertion: Quả quyết với type-system là variable đó có type như vậy
-//! Only be used to more or less specific type; không thể có ~ chuyển đổi vô lý như `"hello" as number`
-//2 `unknown` use case with Type Assertion
-let foo: unknown = 10;
-const upperCase = (x: string) => console.log(x.toUpperCase());
-upperCase(foo as string); //* Assert là string nên type-system ko báo lỗi, lúc run mới ra lỗi TypeError: number ko có method toUpperCase
-
-//2  `as const`: Const Assertion
-const req = { url: "https://example.com", method: "GET" } as const; // object literals get `readonly` properties
-const twoArgFn2 = ([a = 1, b = "b"]) => [a, b] as const; // return type: readonly [number, string]; object literals get `readonly` properties
-const twoArgFn1 = ([a = 1, b = "b"]) => [a, b]; // return type: [string | number][]
-//3 readonly
-interface Home {
-  readonly resident: { name: string; age: number };
-}
-const h: Home = {
-  resident: {
-    name: "John",
-    age: 20,
-  },
-};
-// We can update properties of `home.resident`
-h.resident.name = "Bro";
-// But we can't write to the 'resident' property itself on a 'Home'.
-h.resident = {
-  name: "Victor the Evictor",
-  age: 42,
-};
-
 //1 `unknown`: Similar to `any`, but safer because u can't do anything with it
-let vAny: any = 10;
-let vUnknown: unknown = 10; //* We can assign anything to unknown just like any
+let foo: any = 10;
+let bar: unknown = 10; //* We can assign anything to unknown just like any
+const upperCase = (x: string) => console.log(x.toUpperCase());
+upperCase(foo); // Ok, `any` can assign to anything
+upperCase(bar); //! Invalid; we can't assign `unknown` to any other type (without an explicit assertion)
+upperCase(bar as string); //* Assert là string nên type-system ko báo lỗi, lúc run mới ra lỗi TypeError: number ko có method toUpperCase
 
-let s1: string = vAny; //* Ok; `any` is assignable to anything
-
-let s2: string = vUnknown; //! Invalid; we can't assign `unknown` to any other type (without an explicit assertion)
-let s3: string = vUnknown as string; //* Ok
-
-vAny.method(); //* Ok; anything goes with any
-vUnknown.method(); //! Not ok; we don't know anything about this variable
+foo.method(); //* Ok; anything goes with any
+bar.method(); //! Not ok; we don't know anything about this variable
 
 //1 Utility Types:
 //2 `Record<Keys, Type>`: An object type whose property keys are `Keys` and whose property values are `Type`
@@ -54,8 +24,12 @@ const cats: Record<CatName, CatInfo> = {
 };
 
 //1 Indexed Access Types:
-type Person3 = { age: number; name: string; alive: boolean };
-type Age = Person3["age" | "alive"]; //! Viết là Person3.age -> Lỗi syntax
+const MyArray = [
+  { name: "Alice", age: 15 },
+  { name: "Bob", age: 23 },
+];
+type Person = typeof MyArray[0];
+type Name2 = typeof MyArray[0]["name"];
 
 //1 Conditional Types: Left assignable to Right ? A : B
 type Id = number;
