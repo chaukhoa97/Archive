24871205903c5c8206517708354d545718db4050
Finn Chau <jonhkane00@gmail.com>
2023-10-04 22:40:00 +0700

    Massive delete

--- TypeScript/array_object.ts
+++
@@ -1,8 +0,0 @@
-//2 (Uncommon) instanceof
-function logValue(x: Date | string) {
-  if (x instanceof Date) {
-    console.log(x.toLocaleDateString());
-  } else {
-    console.log(x.toUpperCase());
-  }
-}
